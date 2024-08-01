cdea00d7fca15c50e070cad402bd606e15087423
yunjaehun <wogns8030@naver.com>
2024-08-01 20:12:56 +0900

    feat seaborn

--- pythonai.py
+++ pythonai.py
@@ -10,7 +10,13 @@ cancer = load_breast_cancer();
 # 데이터셋을 DataFrame으로 변환
 df_cancer = pd.DataFrame(np.c_[cancer["data"], cancer["target"]], columns=np.append(cancer["feature_names"], ["target"]));
 
-plt.figure(figsize=(20,10));
-# sns.distplot(df_cancer["mean radius"], bins=25, color= "blue"); #bins 히스토그램의 막대 개수
-sns.histplot(df_cancer["mean radius"], bins=25, color="blue", kde=True) #distplot은 Seaborn 0.11.0 버전 이후로 deprecated
+class_1_df = df_cancer[df_cancer["target"] == 1 ] #target 1만 추출
+class_0_df = df_cancer[df_cancer["target"] == 0 ] #target 0만 추출
+
+print(class_1_df)
+
+plt.figure(figsize=(10,7));
+sns.histplot(class_1_df["mean radius"], bins=25, color="blue", kde=True);
+sns.histplot(class_0_df["mean radius"], bins=25, color="red", kde=True);
+
 plt.show();
