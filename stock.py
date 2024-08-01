c4ec031342801c445c80f886fee33831e6a136c5
yunjaehun <wogns8030@naver.com>
2024-08-01 21:44:21 +0900

    stock_describe()

--- stock.py
+++ stock.py
@@ -3,4 +3,15 @@ import numpy as np
 
 stock_url = "/Users/yunjaehun/desktop/유데미/fi_ai/stocks.csv";
 stock_result = pd.read_csv(stock_url);
-print(stock_result);
+sorting_result = stock_result.sort_values(by=["Date"]); #Sorting Date
+print(sorting_result);
+print(sorting_result.columns[1:]);
+print(len(sorting_result.columns[1:]));
+
+for i in sorting_result.columns[1:]: #주식 col 반환
+    print(i);
+
+print(stock_result["sp500"].mean());
+print(stock_result["sp500"].std());
+print(stock_result.describe()); #통계정보를 제공
+
